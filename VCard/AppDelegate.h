f8d241666040c51b8d9f9282460bb695cc4d973d
Gabriel Yeah <gabriel.yhs@gmail.com>
2012-04-17 14:01:26 +0800

    设置CoreData访问

--- VCard/AppDelegate.h
+++ VCard/AppDelegate.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UIAlertViewDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
 @end
