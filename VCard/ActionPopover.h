e94d6030bf7a02302817b54ccaf57af70d689f76
bluebitch <wzc345@gmail.com>
2012-07-17 14:09:13 +0800

    加入操作按钮素材。微调guide book开始使用按钮的高度。

---
+++ VCard/ActionPopover.h
@@ -0,0 +1,13 @@
+//
+//  ActionPopover.h
+//  VCard
+//
+//  Created by 王 紫川 on 12-7-17.
+//  Copyright (c) 2012年 Mondev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionPopover : UIView
+
+@end
