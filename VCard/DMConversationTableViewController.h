db5f5bd1362165cb75c406c97afb0ed057a730ac
Gabriel Yeah <gabriel.yhs@gmail.com>
2012-07-21 15:39:26 +0800

    修复竖屏进入程序会先变白的问题

---
+++ VCard/DMConversationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMConversationTableViewController.h
+//  VCard
+//
+//  Created by 海山 叶 on 12-7-21.
+//  Copyright (c) 2012年 Mondev. All rights reserved.
+//
+
+#import "RefreshableCoreDataTableViewController.h"
+
+@interface DMConversationTableViewController : RefreshableCoreDataTableViewController
+
+@end
---
+++ VCard/DMConversationTableViewController.m
@@ -0,0 +1,43 @@
+//
+//  DMConversationTableViewController.m
+//  VCard
+//
+//  Created by 海山 叶 on 12-7-21.
+//  Copyright (c) 2012年 Mondev. All rights reserved.
+//
+
+#import "DMConversationTableViewController.h"
+
+@interface DMConversationTableViewController ()
+
+@end
+
+@implementation DMConversationTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
