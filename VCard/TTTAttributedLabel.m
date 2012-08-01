899fe3308e0dd5f0a4c3905c4358638b3f6ccb24
Gabriel Yeah <gabriel.yhs@gmail.com>
2012-08-01 13:29:42 +0800

    调整第一个表情太靠左的问题

--- VCard/TTTAttributedLabel.m
+++ VCard/TTTAttributedLabel.m
@@ -634,7 +634,7 @@ static inline NSAttributedString * NSAttributedStringByScalingFontSize(NSAttribu
     CGFloat leftMargin = shouldShowSmallEmoticon ? 1 : -2;
     CGFloat originY = rect.size.height - rect.origin.y - 7.0;
     CGFloat originX = rect.origin.x + leftMargin;
-    if (originX < 10) {
+    if (originX < 5) {
         originX += 3;
     } else if (originX > 300) {
         originX -= 5;
